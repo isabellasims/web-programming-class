5d79e635021e2e0d60b9dbed20eb0299340c7833
isabellasims <isabellaksims@gmail.com>
2021-03-17 19:13:40 -0400

    proj

--- class/Lecture12Exercises/formexercise/groupizer.js
+++ class/Lecture12Exercises/formexercise/groupizer.js
@@ -11,9 +11,38 @@ Starter code for Groupizer page to demonstrate different types of events,
   // has created the HTML DOM for the page)
   function init() {
     // 1. Add event listener for clicking the #add-btn
+    const SUBMIT = document.getElementById("add-btn");
+
+
+    SUBMIT.addEventListener("click", function(){
+      //alert("sub clicked")
+      showGroup();
+    });
+
+
     // 2. Add event listener for changing the #member-size dropdown
+
   }
 
-  // Add the rest of the functions here!
+  /** Writes group member names to the DOM */
+  function showGroup(){
+    let names = document.getElementsByTagName("input");
+    let p = document.createElement("p");
+    p.innerHTML = "Now the group is formed";
+    document.body.appendChild(p);
+    for(let i = 1; i < names.length; i++){ // exclude group name from loop
+      let p = document.createElement("p");
+      p.innerHTML = names[i].value;
+      document.body.appendChild(p);
+    }
+  }
+
+  function addColor(){
+    let groupForm = document.getElementById("group-name");
+    groupForm.setAttribute("style")
+        style = {backgroundColor: yellow};
+  }
+
+
 
 })();
