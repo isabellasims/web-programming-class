2a939a82be8c120832caace1ba8db1230fda9422
isabellasims <isabellaksims@gmail.com>
2021-03-17 18:01:32 -0400

    ex 12 init

---
+++ class/Lecture12Exercises/formexercise/groupizer.js
@@ -0,0 +1,19 @@
+/**
+Starter code for Groupizer page to demonstrate different types of events,
+ * the module pattern, and DOM manipulation.
+ */
+(function() {
+  "use strict";
+
+  window.addEventListener("load", init);
+
+  // Note: This function is called as soon as the window is loaded (and the browser
+  // has created the HTML DOM for the page)
+  function init() {
+    // 1. Add event listener for clicking the #add-btn
+    // 2. Add event listener for changing the #member-size dropdown
+  }
+
+  // Add the rest of the functions here!
+
+})();
