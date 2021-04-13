e4402d03aab02f1acdf0e0003a82cd6471f80e4d
isabellasims <isabellaksims@gmail.com>
2021-04-12 22:29:29 -0400

    timer fin

--- class/lect15-exercises/typewriter_exercise/typing-simulator.js
+++ class/lect15-exercises/typewriter_exercise/typing-simulator.js
@@ -9,70 +9,57 @@
   window.addEventListener("load", init);
 
     let flag = 1;
-   // let output;
+    let ind = 0;
+
 
 
   function init() {
-    // text = document.querySelector("input").value;
-    // output = document.getElementById("output");
 
-    // id("animate-btn").addEventListener("click", animateText);
+
     id("animate-btn").addEventListener("click",animateText);
-
-
-   //   }
-
-
     id("reset-btn").addEventListener("click", reset);
+
   }
 
   /**
    * Toggles typing animation - if animation is already in progress,
    * pauses it. Otherwise, starts animation.
    */
-  let ind = 0;
+
   function animateText() {
-    //flag = 0;
     let output = document.getElementById("output");
-    // let ind = 0;
 
-
-    // if(flag %2 === 0) {
       window.setInterval(function () {
         // text has to be here so that every time timer ticks it picks up on new input
         let text = document.querySelector("input").value;
 
         if (text[ind] && flag %2 === 0) {
-          // console.log(ind, text.length);
-          // output.innerHTML += text[ind];
-
-            // ind++;
             console.log(ind, text.length);
             output.innerHTML += text[ind];
             ind++;
           }
         else{
+          // Part 2: Change Start to start/stop - what should happen
+          // when we pause animation? Restart at index 0 or keep the current index? ---> kept at current index
           clearInterval();
         }
-
-
-
-
       }, 1000);
-   // }
 
     flag ++;
-    console.log(flag);
 
 
 
 
-    // Part 2: Change Start to start/stop - what should happen
-    // when we pause animation? Restart at index 0 or keep the current index?
+
+
   }
 
   // Part 3: Implement reset: Clear the text, output, and timer.
   function reset() {
+    id("input-text").value = "";
+    id("output").innerHTML = "";
+    ind = 0;
+    clearInterval();
 
   }
 
